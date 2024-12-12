50f38359de31e35c2b54df003e9a5292d5baac42
MukulGautam24 <115268731+MukulGautam24@users.noreply.github.com>
2024-12-12 12:36:20 +0530

    intial commit

--- project1/src/main/java/com/vscodeboot/project1/Project1Application.java
+++ project1/src/main/java/com/vscodeboot/project1/Project1Application.java
@@ -9,6 +9,7 @@ public class Project1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Project1Application.class, args);
 		System.out.println("Hello There this is me Mukul Gautam.");
+		
 	}
 
 }
